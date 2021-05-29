7f0b3f6b75b0d66e40a97f5503dda2874c073272
gonzaloleg <70615605+gonzaloleg@users.noreply.github.com>
2021-05-28 21:03:43 -0300

    ..

--- SolicitudInscripcion/Alumno.cs
+++ SolicitudInscripcion/Alumno.cs
@@ -4,6 +4,17 @@ namespace SolicitudInscripcion
 {
     internal class Alumno
     {
+        public int NumeroRegistro { get; }
+        public string Nombre { get; }
+        public string Apellido { get; }
+        public int DNI { get; }
+        public string Email { get; }
+        public int CodigoCarrera { get; }
+        public bool Ultimas4 { get; }  
+
+
+
+
         internal static void Inicializar()
         {
             throw new NotImplementedException();
